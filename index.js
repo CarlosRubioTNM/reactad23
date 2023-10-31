166dc08575078b1f33a8b4383d1450565ca6ba5e
CarlosRubioTNM <99372099+CarlosRubioTNM@users.noreply.github.com>
2023-10-31 14:53:22 -0600

    Añadir estilos por medio de React

--- index.js
+++ index.js
@@ -21,12 +21,60 @@ function suma(val1, val2) {
 //Insertar variables en React
 //Las expresiones se pueden colocar dentro de llaves para que sean evaluadas.
 //Las sentencias no se pueden colocar.
-let firstName = "Jonathan";
+/*let firstName = "Jonathan";
 let lastName = "Duran";
+const customStyle = {
+    color: "blue",
+    border: "1px solid black"
+}
+customStyle.color = "red";
 ReactDOM.render(
     <div>
-    <h1>Holis</h1>
+    <h1 style={customStyle}>Holis</h1>
     <p>Hola {firstName} {lastName} {suma(null,6)}</p>
-    <p>Copyright {new Date().getFullYear()}. Todos los derechos reservados.</p>
+    <p contentEditable="true" spellCheck="false" className="algo">Copyright {new Date().getFullYear()}. Todos los derechos reservados.</p>
     </div>
-, document.getElementById("asdf"));
+, document.getElementById("asdf"));*/
+
+//Mostrar un mensaje que cambie su contenido y su apariencia
+//dependiendo de la hora del día:
+// - De 5 a 12 hrs, "Ya levántate!" color azul
+// - De 12 a 19 hrs "Ponte a chambear!" color rojo
+// - De 19 a 5 hrs "A mimir!" color negro
+
+let hour = new Date().getHours();
+let msg = "A mimir!";
+const color = {
+    color: "black"
+};
+if (hour > 5 && hour <= 12) {
+    color.color = "blue";
+    msg = "Ya levántate!";
+}
+if (hour > 12 && hour <= 19) {
+    color.color = "red";
+    msg = "Ponte a chambear!";
+}
+ReactDOM.render(
+    <div>
+        <h1>Programa de la hora</h1> 
+        <p style={color}>{msg}</p>
+    </div>, 
+    document.getElementById("asdf"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
